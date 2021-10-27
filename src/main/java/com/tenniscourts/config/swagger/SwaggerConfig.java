a2923b199b52a1f523a0ae3d7444f3bc062cf855
glaucosd <93152274+glaucosd@users.noreply.github.com>
2021-10-27 14:57:27 -0300

    Update SwaggerConfig.java

--- src/main/java/com/tenniscourts/config/swagger/SwaggerConfig.java
+++ src/main/java/com/tenniscourts/config/swagger/SwaggerConfig.java
@@ -18,4 +18,45 @@ public class SwaggerConfig {
                 .paths(PathSelectors.any())
                 .build();
     }
+    
+        private ApiKey apiKey() {
+	    return new ApiKey("mykey", "api_key", "header");
+	}
+	
+	private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Api")
+                .build();
+    }
+	
+	private SecurityContext securityContext() {
+	    return SecurityContext.builder()
+	        .securityReferences(defaultAuth())
+	        .forPaths(PathSelectors.regex("/anyPath.*"))
+	        .build();
+	}
+	
+	List<SecurityReference> defaultAuth() {
+	    AuthorizationScope authorizationScope
+	        = new AuthorizationScope("global", "accessEverything");
+	    AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+	    authorizationScopes[0] = authorizationScope;
+	    return newArrayList(
+	        new SecurityReference("mykey", authorizationScopes));
+	}
+	
+	@Bean
+	SecurityConfiguration security() {
+		return new SecurityConfiguration(
+	        "test-app-guest-id",
+	        "test-app-realm",
+	        "test-app",
+	        "apiKey");
+	}
+     
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 }
